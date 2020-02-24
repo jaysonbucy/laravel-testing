e789469d64e8e603cd9f87998039fde97e8b1d33
Jayson H Bucy <jayson.h.b@gmail.com>
2020-02-23 21:55:26 -0600

    wip

---
+++ database/factories/LinkFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Link;
+use Faker\Generator as Faker;
+
+$factory->define(Link::class, function (Faker $faker) {
+    return [
+        'title' => substr($faker->sentence(2), 0, -1),
+        'url' => $faker->url,
+        'description' => $faker->paragraph,
+    ];
+});
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    $links = \App\Link::all();
+    return view('welcome', ['links' => $links]);
+});
+
+Route::get('/submit', function() {
+    return view('submit');
+});
+
+Route::post('submit', function(Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    $link = tap(new App\Link($data))->save();
+
+    return redirect('/');
+});
